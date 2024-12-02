eb1fd07a6324b5bdd52a0795a5c02cb305dc1d64
hajtohaj <tgabrys@gazeta.pl>
2024-12-02 21:11:11 +0100

    Remove ignored files

--- BlinkingDiode.X/build/default/production/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/main.o: main.c setup_oscilator_35MIPS_FRCPLL.h
-
-setup_oscilator_35MIPS_FRCPLL.h:
--- BlinkingDiode.X/build/default/production/setup_oscilator_35MIPS_FRCPLL.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/setup_oscilator_35MIPS_FRCPLL.o: \
- setup_oscilator_35MIPS_FRCPLL.c setup_oscilator_35MIPS_FRCPLL.h
-
-setup_oscilator_35MIPS_FRCPLL.h:
